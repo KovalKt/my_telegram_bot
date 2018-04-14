04d7a133f2185bd45721d341879fee1302fb1f4b
KovalKt <katrysasonce@gmail.com>
2018-04-14 17:01:35 +0300

    remove sensetive data

--- quotes_bot/quotes_bot.py
+++ quotes_bot/quotes_bot.py
@@ -4,8 +4,7 @@ from config import BOT_TOKEN
 from telegram.ext import Updater, CommandHandler
 from quotes import get_random_quote
 
-# Your bot token (from BotFather)
-token = BOT_TOKEN
+
 PORT = int(os.environ.get('PORT', '8443'))
 
 def start(bot, update):
@@ -19,7 +18,7 @@ def quote(bot, update):
                     text=get_random_quote())
 
 def main():
-    updater = Updater(token);
+    updater = Updater(BOT_TOKEN);
     dp = updater.dispatcher
 
     # Define all the commands that the bot will receive
@@ -29,8 +28,8 @@ def main():
     # add handlers
     updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
-                      url_path=token)
-    updater.bot.set_webhook("https://aqueous-ridge-88309.herokuapp.com/" + token)
+                      url_path=BOT_TOKEN)
+    updater.bot.set_webhook("https://aqueous-ridge-88309.herokuapp.com/" + BOT_TOKEN)
 
     updater.idle()
 
